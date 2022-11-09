b9240a59ecd98524516061279ebb065b028eb962
Elise Mery <elisemery28@gmail.com>
2022-11-09 10:29:20 +0100

    finalisation route search

--- routes/trips.js
+++ routes/trips.js
@@ -1,7 +1,32 @@
 var express = require('express');
 var router = express.Router();
-const Trip= require('../models/trips');
+const Trip = require('../models/trips');
 
+//GET all trips from departure, arrival and date
+router.get('/:departure&:arrival&:date', (req, res) => {
+    
+    const { departure, arrival, date } = req.params
+    
+    let dateInf = new Date(date)
+    dateInf = dateInf.setHours(00, 00, 00)
 
+    let dateSup = new Date(date)
+    dateSup = dateSup.setHours(23, 59, 59)
+
+    Trip.find({
+        departure: departure,
+        arrival: arrival,
+        date: {$gte: dateInf, $lt: dateSup}
+    })
+        .then(trips => {
+            if (trips === []) {
+                res.json({ result: false, error: "no trips found" })
+            }
+            else {
+                res.json({ result: true, trips: trips })
+            }
+        })
+}
+)
 
 module.exports = router;
